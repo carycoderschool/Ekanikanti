e3c32a6d95b82878e28795a8b9d75225e819b666
carycoderschool <42698327+carycoderschool@users.noreply.github.com>
2020-02-17 17:18:27 -0500

    Initial commit

---
+++ Assets/Scripts/GameManager.cs
@@ -0,0 +1,27 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class GameManager : MonoBehaviour
+{
+    public Text scoreText;
+    public int numPickedUp = 0;
+    private int pickUpCount = 0;
+    // Start is called before the first frame update
+    void Start()
+    {
+        pickUpCount = GameObject.FindGameObjectsWithTag("Pick Up").Length;
+
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        scoreText.text = numPickedUp.ToString() + " out of " + pickUpCount.ToString();
+        if(numPickedUp == pickUpCount)
+        {
+            scoreText.text = "You Win!";
+        }
+    }
+}
---
+++ Assets/Scripts/PlayerMovement.cs
@@ -0,0 +1,36 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerMovement : MonoBehaviour
+{
+    public GameManager gameManager;
+    private Rigidbody myRB;
+
+    public float speed = 10f;
+    // Start is called before the first frame update
+    void Start()
+    {
+        myRB = GetComponent<Rigidbody>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        float horMove = Input.GetAxis("Horizontal");
+        float verMove = Input.GetAxis("Vertical");
+
+        Vector3 move = new Vector3(horMove, 0, verMove);
+
+        myRB.AddForce(move * speed * Time.deltaTime);
+    }
+
+    private void OnTriggerEnter(Collider other)
+    {
+        if (other.gameObject.transform.root.tag == "Pick Up")
+        {
+            Destroy(other.gameObject);
+            gameManager.numPickedUp += 1;
+        }
+    }
+}
