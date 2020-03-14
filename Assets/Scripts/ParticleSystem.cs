b61aa7c2a8f5887a41ccb221afaaad602d70f05e
carycoderschool <carycoderschool@gmail.com>
2020-03-14 12:57:38 -0400

    Particles

---
+++ Assets/Scripts/ParticleSystem.cs
@@ -0,0 +1,31 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ParticleSystem : MonoBehaviour
+{
+    // Start is called before the first frame update
+    void Start()
+    {
+        Material particleMaterial = new Material(Shader.Find("Particles/Standard Unlit"));
+
+        var go = new GameObject("Particle System");
+        go.transform.Rotate(-90, 0, 0); // Rotate so the system emits upwards.
+        system = go.AddComponent<ParticleSystem>();
+        go.GetComponent<ParticleSystemRenderer>().material = particleMaterial;
+        var mainModule = system.main;
+        mainModule.startColor = Color.green;
+        mainModule.startSize = 0.5f;
+
+        InvokeRepeating("DoEmit", 2.0f, 2.0f);
+    }
+    public void Emit(ParticleSystem.EmitParams emitParams, int count);
+    void DoEmit()
+    {
+        var emitParams = new ParticleSystem.EmitParams();
+        emitParams.startColor = Color.red;
+        emitParams.startSize = 0.2f;
+        system.Emit(emitParams, 10);
+        system.Play(); // Continue normal emissions
+    }
+}
